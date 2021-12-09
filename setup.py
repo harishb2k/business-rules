932e9c4828eafa0e70226771b0e6fc35730925df
Harish Bohara <harish.bohara@gmail.com>
2021-12-09 23:16:03 +0530

    [x] setup done to run publish on commit

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ with open('HISTORY.rst') as f:
 description = 'Python DSL for setting up business intelligence rules that can be configured without code'
 
 setuptools.setup(
-    name='business-rules-harishbohara',
-    version="0.0.11",
+    name='business-rules-devlibx',
+    version="0.0.1",
     description='{0}\n\n{1}'.format(description, history),
-    author='Harish Bohara',
-    author_email='harish.bohara@gmail.com',
-    url='https://github.com/harishb2k/business-rules',
+    author='devlibx',
+    author_email='devlibx',
+    url='https://github.com/devlibx/business-rules',
     packages=['business_rules'],
     package_dir={"": "."},
     license='MIT'
